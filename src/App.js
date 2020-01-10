4c6b65af3cae84e0d74099eaf636a73079695935
phantrang <phantrang3006@gmail.com>
2020-01-10 12:46:06 +0700

    Test deploy 1

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test deploy. Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Test deploy</p>
         <a
           className="App-link"
           href="https://reactjs.org"
